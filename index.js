dd6f6b1189f99302fa74823cff43592b5fca4db5
Tyler_Holland <tyler.holland@students.makeschool.com>
2021-07-23 13:51:01 -0700

    [update] Torus addition

--- index.js
+++ index.js
@@ -17,3 +17,11 @@ camera.position.setZ(30);
 camera.position.setX(-3);
 
 renderer.render(scene, camera);
+
+// Gemoetryical Torus
+
+const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
+const material = new THREE.MeshStandardMaterial({ color: 0xff6347 });
+const torus = new THREE.Mesh(geometry, material);
+
+scene.add(torus);
