51d2949e82630b043eab82d3ac231e8ec9d2ff9d
Tyler_Holland <tyler.holland@students.makeschool.com>
2021-07-23 20:21:06 -0700

    [update] moon configuration

--- index.js
+++ index.js
@@ -65,3 +65,24 @@ const tylerTexture = new THREE.TextureLoader().load('tyler.png');
 const tyler = new THREE.Mesh(new THREE.BoxGeometry(3, 3, 3), new THREE.MeshBasicMaterial({ map: tylerTexture }));
 
 scene.add(tyler);
+
+// Geometrical Moon
+
+const moonTexture = new THREE.TextureLoader().load('moon.jpg');
+const normalTexture = new THREE.TextureLoader().load('normal.jpg');
+
+const moon = new THREE.Mesh(
+  new THREE.SphereGeometry(3, 32, 32),
+  new THREE.MeshStandardMaterial({
+    map: moonTexture,
+    normalMap: normalTexture,
+  })
+);
+
+scene.add(moon);
+
+moon.position.z = 30;
+moon.position.setX(-10);
+
+tyler.position.z = -5;
+tyler.position.x = 2;
